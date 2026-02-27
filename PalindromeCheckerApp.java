48f95dbe6ebec6e451b1eb78a65aa559562acb40
naithick-step <naithickaccha10@gmail.com>
2026-02-27 16:05:51 +0530

    [naithick] added usecase6 palindromechecker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,22 +1,26 @@
-import java.util.Stack;
+import java.util.*;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-       String word = "noon";
+       String word = "civic";
        Stack<Character> stack = new Stack<>();
+       Queue<Character> queue = new LinkedList<>();
 
-       for (char c : word.toCharArray()) {
-           stack.push(c);
-       }
+        for (char c : word.toCharArray()) {
+              stack.push(c);
+              queue.add(c);
+         }
+        boolean isPalindrome = true;
+            while (!stack.isEmpty()) {
+                if (stack.pop() != queue.remove()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
 
-    boolean isPalindrome = true;
-    for(char c : word.toCharArray()) {
-        if (c != stack.pop()) {
-            isPalindrome = false;
-            break;
-        }
-    }
+
+
     System.out.println("Input text: " + word);
     System.out.println("Is palindrome: " + isPalindrome);
     }
