2929f5cff770b1dc55ca2373460f4dcd61d81297
naithick-step <naithickaccha10@gmail.com>
2026-03-16 14:30:50 +0530

    [naithick] added usecase13 palindromechecker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,64 +1,54 @@
 import java.util.Stack;
-import java.util.ArrayDeque;
-import java.util.Deque;
-
-interface PalindromeStrategy {
-    boolean check(String input);
-}
 
 
-class StackStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String input = "level";
+        
+        System.out.println("Input : " + input);
+
+        
+        long startIterative = System.nanoTime();
+        boolean isPalIterative = checkIterative(input);
+        long endIterative = System.nanoTime();
+        long timeIterative = endIterative - startIterative;
+
       
-        Stack<Character> stack = new Stack<>();
+        long startStack = System.nanoTime();
+        boolean isPalStack = checkStack(input);
+        long endStack = System.nanoTime();
+        long timeStack = endStack - startStack;
 
-  
+       
+        System.out.println("Is Palindrome? : " + isPalIterative);
+        System.out.println("Execution Time (Iterative): " + timeIterative + " ns");
+        System.out.println("Execution Time (Stack-Based): " + timeStack + " ns");
+    }
+
+    public static boolean checkIterative(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static boolean checkStack(String input) {
+        Stack<Character> stack = new Stack<>();
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
-
-
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
                 return false;
             }
         }
         return true;
-    }
-}
-
-
-class DequeStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
-        }
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-
-
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String testInput = "madam";
-        
-      
-        PalindromeStrategy strategy = new StackStrategy();
-        
-        System.out.println("Using Stack Strategy:");
-        System.out.println("Input: " + testInput + " | Result: " + strategy.check(testInput));
-      
-        strategy = new DequeStrategy();
-        
-        System.out.println("\nUsing Deque Strategy (Switched at runtime):");
-        System.out.println("Input: " + testInput + " | Result: " + strategy.check(testInput));
     }
 }
