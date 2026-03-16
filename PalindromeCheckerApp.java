4a52b31aa152ebf2c9b7772b5f282118b93d4e31
naithick-step <naithickaccha10@gmail.com>
2026-03-16 13:51:41 +0530

    [naithick] added usecase9 palindromechecker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -6,25 +6,21 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         
         String input = "level";
-        boolean isPalindrome = true;
+        boolean isPalindrome = check(input, 0, input.length() - 1);
 
-        LinkedList<Character> charList = new LinkedList<>();
 
-        for (char c : input.toCharArray()) {
-            charList.add(c);
-        }
-        while (charList.size() > 1) {
-            char first = charList.removeFirst();
-            char last = charList.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
 
         System.out.println("Input text: " + input);
         System.out.println("Is palindrome: " + isPalindrome);
+    }
+    private static boolean check(String s, int start, int end){
+        if(start >= end){
+            return true;
+        }
+        if(s.charAt(start) != s.charAt(end)){
+            return false;
+        }
+        return check(s, start + 1, end - 1);
     }
 }
 
